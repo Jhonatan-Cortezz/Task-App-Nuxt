9e582551b01bb4cfd814fcf8b8d1d3064df66ebe
Jhonatan Cortez <jhonatancortez708@gmail.com>
2021-01-17 22:32:16 -0600

    rutas dinamicas

---
+++ pages/nosotros.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>Nosotros</h1>
+    <!-- <a href="">Otras pagina</a> -->
+    <router-link to="otra">Otras</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/otra.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    <h2>Otras pagina</h2>
+  </div>
+</template>
